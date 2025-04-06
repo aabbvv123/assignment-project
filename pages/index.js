af314d533cd5ccefa37b4635be5ffd7e35b0d6e9
aabbvv123 <133863004+aabbvv123@users.noreply.github.com>
2025-04-06 13:08:46 +0000

    Fix: Ensure index.js for homepage

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ export default function Home() {
     <div>
       <h1>Firebase Google Login Demo</h1>
       <button>Sign in with Google</button>
+      console.log("Redeploy test");
+      
+console.log("Redeploy check");
+
     </div>
   );
 }
